20cf73d362d785f63aa29a4b6e805e30cfc5268f
Nbiba Bedis <bedisnbiba@gmail.com>
2024-08-07 20:57:10 +0100

    refactor

--- cpu_worker.ts
+++ cpu_worker.ts
@@ -1,8 +1,10 @@
+// @ts-ignore - This is a worker
 self.onmessage = (e: MessageEvent) => {
   const cpuNumber = e.data.cpu;
   console.log(`CPU${cpuNumber} worker started`);
 
   // Infinite loop to max out CPU
   while (true) {
+    // Keep the CPU busy
   }
 };
--- main.ts
+++ main.ts
@@ -1,6 +1,19 @@
 import * as slint from "npm:slint-ui@1.7.1";
 
-function tmp_dir(): string | null {
+interface Window {
+  cpu_data: CPUData[];
+  active_cpus: number;
+  toggleCPU: (cpuNumber: number) => boolean;
+
+  run: () => Promise<void>;
+}
+
+interface CPUData {
+  number: number;
+  active: boolean;
+}
+
+function tmpDir(): string | null {
   switch (Deno.build.os) {
     case "linux": {
       return "/tmp";
@@ -13,48 +26,29 @@ function tmp_dir(): string | null {
   return null;
 }
 
-// TODO: embed the svg so I don't need this hack
-const imageUrlPath = (tmp_dir() ?? "/tmp") + "/burncpu-fire.svg";
-await fetch(import.meta.resolve("./fire.svg")).then((r) =>
-  r.body?.pipeTo(
-    Deno.openSync(imageUrlPath, {
-      write: true,
-      create: true,
-    }).writable,
-  )
-);
-
-// Import our Slint UI
-// TODO: same hack part 2
-const uiData = await fetch(import.meta.resolve("./ui.slint"))
-  .then((r) => r.text())
-  .then((r) => r.replace("IMAGE_URL_PATH", imageUrlPath));
-
-const ui = slint.loadSource(uiData, "main.js");
-
-const window = new ui.Window();
-
-// Get the number of logical CPUs
-const numCPUs = navigator.hardwareConcurrency || 4; // Fallback to 4 if not available
-
-const workers: { [key: number]: Worker } = {};
-
 // Function to start a CPU-intensive worker
-function startCPUWorker(cpuNumber: number) {
+function startCPUWorker(
+  workers: { [key: number]: Worker },
+  cpuNumber: number,
+) {
   if (workers[cpuNumber]) {
     console.log(`CPU${cpuNumber} worker already running`);
     return;
   }
-  const worker = new Worker(new URL("./cpu_worker.ts", import.meta.url).href, {
-    type: "module",
-  });
+  const worker = new Worker(
+    import.meta.resolve("./cpu_worker.ts"),
+    { type: "module" },
+  );
   worker.postMessage({ cpu: cpuNumber });
   workers[cpuNumber] = worker;
   console.log(`Started CPU${cpuNumber} worker`);
 }
 
 // Function to stop a CPU worker
-function stopCPUWorker(cpuNumber: number) {
+function stopCPUWorker(
+  workers: { [key: number]: Worker },
+  cpuNumber: number,
+) {
   if (workers[cpuNumber]) {
     workers[cpuNumber].terminate();
     delete workers[cpuNumber];
@@ -63,42 +57,73 @@ function stopCPUWorker(cpuNumber: number) {
     console.log(`No active worker for CPU${cpuNumber}`);
   }
 }
+
 function stopAllWorkers(workers: { [key: number]: Worker }) {
   Object.keys(workers).forEach((cpuNumber) => {
-    stopCPUWorker(parseInt(cpuNumber));
+    stopCPUWorker(workers, parseInt(cpuNumber));
   });
 }
 
-// Generate CPU data for the UI
-const cpuData = Array.from({ length: numCPUs }, (_, i) => ({
-  number: i + 1,
-  active: false,
-}));
-
-// Set the CPU data in the UI
-window.cpu_data = cpuData;
-
 // Function to update active CPU count
-function updateActiveCPUs() {
+function updateActiveCPUs(window: Window, cpuData: CPUData[]) {
   const activeCPUs = cpuData.filter((cpu) => cpu.active).length;
   window.active_cpus = activeCPUs;
 }
 
-// Initial update of active CPUs
-updateActiveCPUs();
+if (import.meta.main) {
+  // TODO: embed the svg so I don't need this hack
+  const imageUrlPath = (tmpDir() ?? "/tmp") + "/burncpu-fire.svg";
+  await fetch(import.meta.resolve("./fire.svg"))
+    .then((r) =>
+      r.body?.pipeTo(
+        Deno.openSync(imageUrlPath, {
+          write: true,
+          create: true,
+        }).writable,
+      )
+    );
 
-// Connect UI signals to worker creation and termination
-window.toggleCPU = (cpuNumber: number) => {
-  if (workers[cpuNumber]) {
-    stopCPUWorker(cpuNumber);
-    cpuData[cpuNumber - 1].active = false;
-  } else {
-    startCPUWorker(cpuNumber);
-    cpuData[cpuNumber - 1].active = true;
-  }
-  updateActiveCPUs();
-  return cpuData[cpuNumber - 1].active;
-};
+  // Import our Slint UI
+  // TODO: same hack part 2
+  const uiData = await fetch(import.meta.resolve("./ui.slint"))
+    .then((r) => r.text())
+    .then((r) => r.replace("IMAGE_URL_PATH", imageUrlPath));
 
-await window.run();
-stopAllWorkers(workers);
+  const ui = slint.loadSource(uiData, "main.js");
+
+  // deno-lint-ignore no-explicit-any
+  const window = new (ui as any).Window() as Window;
+
+  // Get the number of logical CPUs
+  const numCPUs = navigator.hardwareConcurrency || 4; // Fallback to 4 if not available
+
+  const workers: { [key: number]: Worker } = {};
+
+  // Generate CPU data for the UI
+  const cpuData = Array.from({ length: numCPUs }, (_, i) => ({
+    number: i + 1,
+    active: false,
+  }));
+
+  // Set the CPU data in the UI
+  window.cpu_data = cpuData;
+
+  // Initial update of active CPUs
+  updateActiveCPUs(window, cpuData);
+
+  // Connect UI signals to worker creation and termination
+  window.toggleCPU = (cpuNumber: number) => {
+    if (workers[cpuNumber]) {
+      stopCPUWorker(workers, cpuNumber);
+      cpuData[cpuNumber - 1].active = false;
+    } else {
+      startCPUWorker(workers, cpuNumber);
+      cpuData[cpuNumber - 1].active = true;
+    }
+    updateActiveCPUs(window, cpuData);
+    return cpuData[cpuNumber - 1].active;
+  };
+
+  await window.run();
+  stopAllWorkers(workers);
+}
