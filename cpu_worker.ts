85d34ace64458d315cb58ecf0481ca703844287c
Nbiba Bedis <bedisnbiba@gmail.com>
2025-07-03 08:13:13 +0100

    Add .gitignore for burncpu and update worker typings

--- cpu_worker.ts
+++ cpu_worker.ts
@@ -1,7 +1,6 @@
-// @ts-ignore - This is a worker
+/// <reference lib="webworker" />
 self.onmessage = (e: MessageEvent) => {
   const _cpuNumber = e.data.cpu; // Prefix with underscore to indicate intentionally unused
-
 
   // Infinite loop to max out CPU
   while (true) {
